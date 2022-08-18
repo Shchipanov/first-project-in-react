b33df492a7b073dd2344aa88b2e0f7be1aabb310
Shchipanov <97899360+Shchipanov@users.noreply.github.com>
2022-08-18 09:01:45 +0400

    делает компонент default

---
+++ src/thems/default.js
@@ -0,0 +1,15 @@
+export const defaultTheme = {
+  colorBlackForText: "#333333",
+  colorWhite: "#ffffff",  
+  colorForFeature: "#E1EDCE",
+  colorForFeatureNegative: "#F8DDD7",
+  colorForFeatureOwner: "#88AA4D",
+  colorForFeatureOwneNegative: "#F75531",
+  colorForButton: "#FC9B27",
+  colorForButtonHover: "#FC7427",
+  pagePadding: "90px",
+  fontFamily: '"Inter", "Arial", sans-serif',
+  fontSizeDefault: "18px",
+  footerHeight: "80px",
+  pageWidth: "1280px"
+};
