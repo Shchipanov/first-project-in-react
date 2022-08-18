b336a66084529cf8f17751c58967dcb0993be561
Shchipanov <97899360+Shchipanov@users.noreply.github.com>
2022-08-18 15:25:52 +0400

    Делает стилизацию кнопок. только размер не сработал

---
+++ src/components/ui/button/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.a`
+  display: block;
+  min-height: 60px;
+  font-size: ${(props) => props.theme.fontSizeDefault};
+  min-width: ${(props) => `${props.$minWidth}px` || "100%"};
+  max-width: 700px;
+  font-weight: 700;
+  line-height: 1.5;
+  color: ${(props) => props.theme.colorWhite};
+  background-color: ${(props) => props.theme.colorForButton};
+  text-align: center;
+  text-decoration: none;
+  border: none;
+  background-image: none;
+  cursor: pointer;
+  box-shadow: none;
+  transition: background-color 0.2s ease-out, box-shadow 0.2s ease-out;
+
+  &:hover,
+  &:active {
+    background-color: ${(props) => props.theme.colorForButtonHover};
+    box-shadow: inset 0 4px 0 rgba(0, 0, 0, 0.14);
+  }
+
+  &:active {
+    box-shadow: none;
+  }
+`;
