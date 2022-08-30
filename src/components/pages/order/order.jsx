68cfa215a67e08723bdeaad97fcbdd8a7d5ea6a1
Shchipanov <97899360+Shchipanov@users.noreply.github.com>
2022-08-30 16:26:26 +0400

    делает Обработка события покупки

--- src/components/pages/order/order.jsx
+++ src/components/pages/order/order.jsx
@@ -39,6 +39,15 @@ function Order({
       swiperRef.slideTo(index, 0);
     }
   };
+  const [address, setAddress] = useState("");
+  const handleBuyClick = () => {
+    // eslint-disable-next-line no-alert
+    alert(`Спасибо за заказ, вы купили:\n${selectProducts.map(
+      (product) => `${product.name} - ${product.price} руб.\n`
+    )}
+    Итого: ${fullPrice} руб.
+    Доставка по адресу: ${address}.`);
+  };
   return (
     <StyledOrder as="form">
       <LeftColumn>
@@ -63,10 +72,16 @@ function Order({
           <Title size={TitleSize.EXTRA_SMALL} marginBottom={24}>
             Сделать заказ
           </Title>
-          <AddressInput placeholder="Введите адрес доставки" />
+          <AddressInput
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            placeholder="Введите адрес доставки"
+          />
           <PriceLabel as="span">Цена</PriceLabel>
           <PriceValue value={fullPrice} />
-          <Button maxWidth>Купить</Button>
+          <Button maxWidth onClick={handleBuyClick}>
+            Купить
+          </Button>
         </Panel>
       </LeftColumn>
       <ProductSwiper
