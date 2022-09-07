547e1ed4dcca2f61cbc0b50cc443aac685421a31
Shchipanov <97899360+Shchipanov@users.noreply.github.com>
2022-09-07 11:25:10 +0400

    Починка скрола

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,12 +7,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AppRoute } from "/src/const";
 import MainPage from "/src/components/pages/main-page/main-page";
 import Order from "/src/components/pages/order/order";
+import ScrollTop from "/src/components/ui/scroll-top/scroll-top";
 
 export default function App() {
   return (
     <>
       <GlobalStyle />
       <Router>
+        <ScrollTop />
         <Routes>
           <Route path={AppRoute.MAIN} element={<PageWrapper />}>
             <Route index element={<MainPage features={features} />} />
