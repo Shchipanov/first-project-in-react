0802e9dd01c3560297b8d7bddb4d140bd2cc2903
Shchipanov <97899360+Shchipanov@users.noreply.github.com>
2022-09-07 10:57:36 +0400

    Заменяет кнопки в шапке

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -3,6 +3,7 @@ import Logo from "/src/components/ui/logo/logo";
 import Nav from "/src/components/layout/nav/nav";
 import { StyledSection } from "./styles";
 
+// шапка сайта
 function Header() {
   return (
     <StyledSection as="header">
